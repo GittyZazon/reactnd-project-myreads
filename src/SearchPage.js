03b3a38756c74c8ed8f318e5feac5bc6dff2d8b2
Gitty Zazon <gittyzazon@gmail.com>
2018-08-29 14:32:13 +0300

    Change default value of shelf changer to current shelf of book

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -50,7 +50,7 @@ class SearchPage extends Component {
 					        <div className="book-top">
 					          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
 					          <div className="book-shelf-changer">
-					            <select onChange={this.handleChange} defaultValue="move">
+					            <select onChange={this.handleChange} defaultValue={book.shelf}>
 					              <option value="move" disabled>Move to...</option>
 					              <option value="currentlyReading">Currently Reading</option>
 					              <option value="wantToRead">Want to Read</option>
